8f4e1e194cca6f95843d123ef54358858c78333d
Olof Tingskull <olof.tingskull@gmail.com>
2024-02-25 19:12:05 +0100

    offer thing

--- src/problems/caveat_venditor.rs
+++ src/problems/caveat_venditor.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashSet, iter::once};
+
 
 #[derive(Debug, Clone, Copy)]
 pub struct Offer {
@@ -5,32 +7,86 @@ pub struct Offer {
     buy: usize,
 }
 
+#[derive(Debug, Clone)]
+struct Payment {
+    pay_for_amount: usize,
+    using_offers: HashSet<usize>,
+}
 
 #[allow(dead_code)]
-pub fn run(total_to_buy: usize, offers: Vec<Offer>, price_per_movie: f64) -> f64 {
-    let mut min_cost_for_each_quantity = vec![0; total_to_buy + 1];
-    let mut used_offsers = vec![false; offers.len()];
+pub fn run(total_to_buy: usize, offers: &Vec<Offer>, price_per_movie: f64) -> f64 {
+    let mut quantity_payment_dp = (0..=total_to_buy)
+        .map(|i| Payment { pay_for_amount: i, using_offers: HashSet::new() })
+        .collect::<Vec<_>>();
 
-    for i in 1..=total_to_buy {
-        let mut min_cost_for_current_quantity = i;
-        let mut offer_used_index: Option<usize> = None;
+    for current_quentity in 1..=total_to_buy {
+        let no_offer_payment = {
+            let mut p = quantity_payment_dp[current_quentity - 1].clone();
+            p.pay_for_amount += 1;
+            p
+        };
 
+         let payment_options: Vec<_> = offers
+            .iter()
+            .enumerate()
+            .filter_map(|(index, offer)| {
+                if offer.buy >= current_quentity {
+                    let mut used_offers = HashSet::new();
+                    used_offers.insert(index);
 
-        for (j, offer) in offers.iter().enumerate() {
-            if used_offsers[j] { continue; }
-            let cost_with_offer = min_cost_for_each_quantity[i - offer.buy] + offer.buy - offer.free;
-            if cost_with_offer < min_cost_for_current_quantity {
-                min_cost_for_current_quantity = cost_with_offer;
-                offer_used_index = Some(j);
-            }
-        }
+                    let p =  Payment {
+                        pay_for_amount: offer.buy - offer.free,
+                        using_offers: used_offers,
+                    };
 
-        if let Some(offer_index) = offer_used_index {
-            used_offsers[offer_index] = true;
-        }
+                    return Some(p);
+                } else {
+                    let remaining_payment = &quantity_payment_dp[current_quentity - offer.buy];
+                    if remaining_payment.using_offers.contains(&index) {
+                        return None;
+                    }
 
-        min_cost_for_each_quantity[i] = min_cost_for_current_quantity;
+                    let mut used_offers = remaining_payment.using_offers.clone();
+                    used_offers.insert(index);
+
+                    let p =  Payment {
+                        pay_for_amount: remaining_payment.pay_for_amount + offer.buy - offer.free,
+                        using_offers: used_offers,
+                    };
+
+                    return Some(p);
+                }
+            })
+            .chain(once(no_offer_payment))
+            .collect();
+
+        let best_payment = payment_options
+            .iter()
+            .min_by(|a, b| a.pay_for_amount.cmp(&b.pay_for_amount))
+            .unwrap();
+
+        quantity_payment_dp[current_quentity] = best_payment.clone();
     }
 
-    min_cost_for_each_quantity[total_to_buy] as f64 * price_per_movie
+    quantity_payment_dp[total_to_buy].pay_for_amount as f64 * price_per_movie
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_offers() {
+        use super::*;
+
+        let offers = vec![
+            Offer { free: 2, buy: 4 },
+            Offer { free: 5, buy: 9 },
+            Offer { free: 2, buy: 3 },
+        ];
+
+        let price_per_movie = 99.;
+
+        for i in 1..=18 {
+            println!("{}", run(i, &offers, price_per_movie));
+        }
+    }
 }
--- src/problems/quid_pro_quo.rs
+++ src/problems/quid_pro_quo.rs
@@ -1,7 +1,13 @@
-use std::{char, vec};
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashSet;
+use std::hash::{Hash, Hasher};
 
-fn get_surplus (resources: &Vec<i32>, goals: &Vec<i32>) -> Vec<i32> {
-    resources.iter().zip(goals.iter()).map(|(r, g)| r - g).collect()
+fn get_surplus(resources: &Vec<i32>, goals: &Vec<i32>) -> Vec<i32> {
+    resources
+        .iter()
+        .zip(goals.iter())
+        .map(|(r, g)| r - g)
+        .collect()
 }
 
 #[derive(Debug)]
@@ -11,38 +17,95 @@ struct Trade {
     give: usize,
 }
 
-#[allow(dead_code)]
-pub fn run (player_resources: Vec<i32>, character_resources: Vec<Vec<i32>>, character_goals: Vec<Vec<i32>>, depth: usize) -> bool {
+struct State {
+    player_resources: Vec<i32>,
+    character_resources: Vec<Vec<i32>>,
+    character_goals: Vec<Vec<i32>>,
+}
+
+impl State {
+    fn hash(&self) -> u64 {
+        let mut hasher = DefaultHasher::new();
+
+        for &resource in &self.player_resources {
+            (resource > 0).hash(&mut hasher);
+        }
+
+        for character_resource in &self.character_resources {
+            for &resource in character_resource {
+                resource.hash(&mut hasher);
+            }
+        }
+
+        for character_goal in &self.character_goals {
+            for &goal in character_goal {
+                goal.hash(&mut hasher);
+            }
+        }
+
+        hasher.finish()
+    }
+}
+
+fn search(
+    state: State,
+    explored_states: &mut HashSet<u64>,
+    successful_trades: &mut Vec<Trade>,
+) -> bool {
+    explored_states.insert(state.hash());
+
+    let State {
+        player_resources,
+        character_resources,
+        character_goals,
+    } = state;
+
     let n = player_resources.len();
     let m = character_resources.len();
-    if player_resources.iter().sum::<i32>() < n as i32 { return false; }
-    if player_resources.iter().all(|&r| r != 0) { return true; }
+    if player_resources.iter().sum::<i32>() < n as i32 {
+        return false;
+    }
+    if player_resources.iter().all(|&r| r != 0) {
+        return true;
+    }
 
     let mut possible_trades = Vec::<Trade>::new();
 
-    let character_surplus: Vec<Vec<i32>> = (0..m).map(|i| get_surplus(&character_resources[i], &character_goals[i])).collect();
-
+    let character_surplus: Vec<Vec<i32>> = (0..m)
+        .map(|i| get_surplus(&character_resources[i], &character_goals[i]))
+        .collect();
 
     for i in 0..n {
         let player_get = player_resources[i];
-        if player_get != 0 { continue; }
+        if player_get != 0 {
+            continue;
+        }
 
         let mut can_trade = false;
 
-        for j in 0..n { 
+        for j in 0..n {
             let player_give = player_resources[j];
-            if player_give <= 1 { continue; }
+            if player_give < 1 {
+                continue;
+            }
 
             for k in 0..m {
                 if character_surplus[k][i] > 0 && character_surplus[k][j] < 0 {
-                    let trade = Trade { with: k, get: i, give: j };
+                    let trade = Trade {
+                        with: k,
+                        get: i,
+                        give: j,
+                    };
+
                     possible_trades.push(trade);
                     can_trade = true;
                 }
             }
         }
 
-        if !can_trade { return false; }
+        if !can_trade {
+            return false;
+        }
     }
 
     for trade in possible_trades {
@@ -64,21 +127,50 @@ pub fn run (player_resources: Vec<i32>, character_resources: Vec<Vec<i32>>, char
 
         let character_goals_after_trade = character_goals.clone();
 
-        let could_trade = run(
-            player_resource_after_trade, 
-            character_resource_after_trade, 
-            character_goals_after_trade,
-            depth + 1
-        );
+        let new_state = State {
+            player_resources: player_resource_after_trade,
+            character_resources: character_resource_after_trade,
+            character_goals: character_goals_after_trade,
+        };
+
+        if explored_states.contains(&new_state.hash()) {
+            continue;
+        }
+        let could_trade = search(new_state, explored_states, successful_trades);
 
         if could_trade {
+            successful_trades.push(trade);
             return true;
         }
     }
 
-    return false
+    return false;
 }
 
+#[allow(dead_code)]
+pub fn run(
+    player_resources: Vec<i32>,
+    character_resources: Vec<Vec<i32>>,
+    character_goals: Vec<Vec<i32>>,
+) -> bool {
+    let state = State {
+        player_resources,
+        character_resources,
+        character_goals,
+    };
+
+    let mut explored_states = HashSet::new();
+    let mut successful_trades = Vec::new();
+
+    let res = search(state, &mut explored_states, &mut successful_trades);
+
+    if !res {
+        println!("successful_trades: {:?}", successful_trades.len());
+        println!("explored_states: {:?}", explored_states.len());
+    }
+
+    res
+}
 
 #[cfg(test)]
 mod tests {
@@ -88,14 +180,27 @@ mod tests {
     #[test]
     fn test_trade() {
         let mut rng = rand::thread_rng();
-        let n = 100;
-        let m = 100;
+        let n = 10;
+        let m = 5;
 
-        let player_resources: Vec<i32> = (0..n).map(|_| if rng.gen_bool(0.5) { rng.gen_range(1..5) } else { 0 } ).collect();
-        let character_resources: Vec<Vec<i32>> = (0..m).map(|_| (0..n).map(|_| rng.gen_range(0..n as i32)).collect()).collect();
-        let character_goals: Vec<Vec<i32>> = (0..m).map(|_| (0..n).map(|_| rng.gen_range(0..n as i32)).collect()).collect();
+        loop {
+            let player_resources: Vec<i32> = (0..n)
+                .map(|_| {
+                    if rng.gen_bool(0.5) {
+                        rng.gen_range(1..5)
+                    } else {
+                        0
+                    }
+                })
+                .collect();
+            let character_resources: Vec<Vec<i32>> = (0..m)
+                .map(|_| (0..n).map(|_| rng.gen_range(0..n as i32)).collect())
+                .collect();
+            let character_goals: Vec<Vec<i32>> = (0..m)
+                .map(|_| (0..n).map(|_| rng.gen_range(0..n as i32)).collect())
+                .collect();
 
-        let result = run(player_resources, character_resources, character_goals, 0);
-        println!("RESULT: {}", result);
+            let _ = run(player_resources, character_resources, character_goals);
+        }
     }
 }
